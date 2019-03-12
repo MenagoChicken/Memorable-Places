7fbb3e5a0001f6f0a0f2380ba49a33eca89f6a2f
MenagoChicken <lewandowski.michau@gmail.com>
2019-03-12 22:57:00 +0100

    setup list

--- app/src/main/java/pl/menagochicken/memorableplaces/MainActivity.java
+++ app/src/main/java/pl/menagochicken/memorableplaces/MainActivity.java
@@ -1,13 +1,45 @@
 package pl.menagochicken.memorableplaces;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Intent intent;
+    private List<String> memorablePlacesList;
+    private ListView memorablePlacesListView;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        memorablePlacesList= new ArrayList<>();
+        memorablePlacesList.add("Add new place");
+
+        memorablePlacesListView = findViewById(R.id.MemorblePlacesList);
+
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,memorablePlacesList);
+
+        memorablePlacesListView.setAdapter(arrayAdapter);
+
+        memorablePlacesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
